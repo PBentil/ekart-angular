5f87e82bb7192092092ecf4d9f7aa78ec45d46c4
PBentil <paapabentil122@gmail.com>
2025-10-04 14:00:59 +0000

    creating and rendering components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  standalone : true,
 })
 export class AppComponent {
   title = 'Ecommerce-app';
 }
+  
